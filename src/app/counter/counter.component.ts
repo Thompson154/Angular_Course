2323ef06df6a2e28246a06555f9b5952617972ed
Adrian Thompson <thompsonadrian210@gmail.com>
2024-10-07 20:25:04 -0400

    [10/07/2024] ngStyleClass and ngModule

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -11,41 +11,41 @@ export class CounterComponent {
   sum: number = 0;
   appBackground: string = 'red'
 
-  constructor() {
-    afterRender({
-    //  earlyRead: () => {
-      // const currentAppColor = this.appBackground
-      // console.log("Llegaste al earlyRead " + this.appBackground)
-      //   return 'From earlyRead: ' + currentAppColor
-      // },
-      // mixedReadWrite: (props) => {
-      //   if(props.includes('red')){
-      //     this.appBackground = 'green'
-      //   } else {
-      //     this.appBackground = 'red'
-      //   }
-      //   console.log("Llegaste al mixedReadWrite " + this.appBackground)
-      //   return 'From mixedReadWrite: ' + this.appBackground
-      // },
-      write: () => {
-        document.body.style.backgroundColor = this.appBackground
-        const currentColor = this.appBackground
-        if(currentColor ==='red'){
-          this.appBackground = 'blue'
-        } else {
-          this.appBackground = 'red'
-        }
-        console.log("Llegaste al WRITE " + this.appBackground)
-        return 'FROM write:' + this.appBackground
-      },
-      read: (props) => {
-        const newBackground = this.appBackground
-        console.log("Llegaste al READ: " + this.appBackground)
-      }
-    })
+  // constructor() {
+  //   afterRender({
+  //   //  earlyRead: () => {
+  //     // const currentAppColor = this.appBackground
+  //     // console.log("Llegaste al earlyRead " + this.appBackground)
+  //     //   return 'From earlyRead: ' + currentAppColor
+  //     // },
+  //     // mixedReadWrite: (props) => {
+  //     //   if(props.includes('red')){
+  //     //     this.appBackground = 'green'
+  //     //   } else {
+  //     //     this.appBackground = 'red'
+  //     //   }
+  //     //   console.log("Llegaste al mixedReadWrite " + this.appBackground)
+  //     //   return 'From mixedReadWrite: ' + this.appBackground
+  //     // },
+  //     write: () => {
+  //       document.body.style.backgroundColor = this.appBackground
+  //       const currentColor = this.appBackground
+  //       if(currentColor ==='red'){
+  //         this.appBackground = 'blue'
+  //       } else {
+  //         this.appBackground = 'red'
+  //       }
+  //       console.log("Llegaste al WRITE " + this.appBackground)
+  //       return 'FROM write:' + this.appBackground
+  //     },
+  //     read: (props) => {
+  //       const newBackground = this.appBackground
+  //       console.log("Llegaste al READ: " + this.appBackground)
+  //     }
+  //   })
 
-    afterNextRender(()=>{
-      console.log("After NEXT RENDER:", this.sum)
-    })
-  }
+  //   afterNextRender(()=>{
+  //     console.log("After NEXT RENDER:", this.sum)
+  //   })
+  // }
 }
